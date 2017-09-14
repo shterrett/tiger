5b095a97ab515a92556bed657ac4fb81300d82a7
Stuart Terrett <shterrett@gmail.com>
2017-09-14 08:57:27 -0400

    Max number of args to PrintStm

--- src/Straightline.hs
+++ src/Straightline.hs
@@ -9,7 +9,7 @@ data Binop =
 
 data Exp =
     IdExp Id
-    | NumExp Integer
+    | NumExp Int
     | OpExp Exp Binop Exp
     | EseqExp Stm Exp
 
@@ -18,5 +18,18 @@ data Stm =
     | AssignStm Id Exp
     | PrintStm [Exp]
 
-maxargs :: Stm -> Integer
-maxargs s = 0
+maxargs :: Stm -> Int
+maxargs s = stmMaxPrintArgs 0 s
+
+stmMaxPrintArgs :: Int -> Stm -> Int
+stmMaxPrintArgs m (CompoundStm s1 s2) = max m $ max (stmMaxPrintArgs m s1) (stmMaxPrintArgs m s2)
+stmMaxPrintArgs m (PrintStm es) = max m $
+                                  max (length es) (maximum $ (expMaxPrintArgs m) <$> es)
+stmMaxPrintArgs m (AssignStm _ e) = max m $ expMaxPrintArgs m e
+
+expMaxPrintArgs :: Int -> Exp -> Int
+expMaxPrintArgs m (IdExp _) = m
+expMaxPrintArgs m (NumExp _) = m
+expMaxPrintArgs m (OpExp e1 _ e2) = max m $
+                                        max (expMaxPrintArgs m e1) (expMaxPrintArgs m e2)
+expMaxPrintArgs m (EseqExp s e) = max (stmMaxPrintArgs m s) (expMaxPrintArgs m e)
--- test/StraightlineSpec.hs
+++ test/StraightlineSpec.hs
@@ -9,5 +9,22 @@ spec =
       it "returns the number of arguments in a print statement" $ do
         let statement = PrintStm [ NumExp(5), NumExp(6), NumExp(7) ]
         maxargs statement `shouldBe` 3
-
-
+      it "returns 0 if there is no print statement" $ do
+        let statement = AssignStm "var" (NumExp 5)
+        maxargs statement `shouldBe` 0
+      it "returns the maximum number of args when multiple print statements exist" $ do
+        let statement =
+              CompoundStm
+                (PrintStm [NumExp 5])
+                (AssignStm "var"
+                           (OpExp
+                             (NumExp 5)
+                             Plus
+                             (EseqExp (PrintStm [NumExp 6, NumExp 7, NumExp 8]) (NumExp 9))))
+        maxargs statement `shouldBe` 3
+      it "returns the maximum number of args when print statements are nested" $ do
+        let statement =
+              PrintStm [ EseqExp (PrintStm [ NumExp 1, NumExp 2, NumExp 3 ])
+                                 (NumExp 4)
+                       , NumExp 5 ]
+        maxargs statement `shouldBe` 3
