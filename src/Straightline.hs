3ebd8c54b7bde8408cf9800b672a8b50387bcf56
Stuart Terrett <shterrett@gmail.com>
2017-09-14 09:53:38 -0400

    Interpret straightline language

--- src/Straightline.hs
+++ src/Straightline.hs
@@ -1,5 +1,9 @@
 module Straightline where
 
+import qualified Data.Map.Strict as Map
+import Data.Map.Strict ( (!) )
+import Data.Foldable (foldl')
+
 type Id = String
 data Binop =
     Plus
@@ -18,6 +22,12 @@ data Stm =
     | AssignStm Id Exp
     | PrintStm [Exp]
 
+type Store = Map.Map Id [Int]
+type Output = [Int]
+type Env = (Store, Output)
+
+--------------------------------------------------------------------------------
+
 maxargs :: Stm -> Int
 maxargs s = stmMaxPrintArgs 0 s
 
@@ -33,3 +43,46 @@ expMaxPrintArgs m (NumExp _) = m
 expMaxPrintArgs m (OpExp e1 _ e2) = max m $
                                         max (expMaxPrintArgs m e1) (expMaxPrintArgs m e2)
 expMaxPrintArgs m (EseqExp s e) = max (stmMaxPrintArgs m s) (expMaxPrintArgs m e)
+
+--------------------------------------------------------------------------------
+
+envStore :: Env -> Id -> Int -> Env
+envStore (store, out) id int = (Map.insertWith (++) id [int] store, out)
+
+envLookup :: Env -> Id -> Int
+envLookup (store, _) id = head (store ! id)
+
+printExp :: Env -> Exp -> Env
+printExp (store, out) exp =
+    let ((s1, o1), val) = interpExp (store, out) exp
+    in (s1, o1 ++ [val])
+
+operate :: Binop -> Int -> Int -> Int
+operate Plus = (+)
+operate Minus = (-)
+operate Times = (*)
+operate Div = div
+
+interp :: Stm -> IO ()
+interp stm = let (_, output) = interpStm (Map.empty, []) stm
+             in (putStrLn . show) output
+
+interpStm :: Env -> Stm -> Env
+interpStm e (CompoundStm s1 s2) = let e2 = interpStm e s1
+                                  in interpStm e2 s2
+interpStm e (AssignStm id exp) =
+    let (e2, val) = interpExp e exp
+    in envStore e2 id val
+interpStm (store, out) (PrintStm exps) =
+    foldl' printExp (store, out) exps
+
+interpExp :: Env -> Exp -> (Env, Int)
+interpExp e (IdExp id) = (e, envLookup e id)
+interpExp e (NumExp int) = (e, int)
+interpExp e (OpExp exp1 op exp2) =
+    let (_, val1) = interpExp e exp1
+        (_, val2) = interpExp e exp2
+    in (e, operate op val1 val2)
+interpExp e (EseqExp stm exp) =
+    let e2 = interpStm e stm
+    in interpExp e2 exp
--- test/StraightlineSpec.hs
+++ test/StraightlineSpec.hs
@@ -1,10 +1,12 @@
 module StraightlineSpec where
 
 import Test.Hspec
+import qualified Data.Map.Strict as Map
+
 import Straightline
 
 spec :: Spec
-spec =
+spec = do
     describe "maxargs" $ do
       it "returns the number of arguments in a print statement" $ do
         let statement = PrintStm [ NumExp(5), NumExp(6), NumExp(7) ]
@@ -28,3 +30,73 @@ spec =
                                  (NumExp 4)
                        , NumExp 5 ]
         maxargs statement `shouldBe` 3
+
+    describe "interpExp" $ do
+      it "evaluates a simple lookup" $ do
+        let env = (Map.singleton "a" [3], [])
+            statement = IdExp "a"
+        interpExp env statement `shouldBe` (env, 3)
+      it "evaluates a number literal" $ do
+        let env = (Map.empty, [])
+            statement = NumExp 5
+        interpExp env statement `shouldBe` (env, 5)
+      it "evaluates an arithmatic operation" $ do
+        let env = (Map.empty, [])
+            statement = OpExp (NumExp 5) Plus (NumExp 8)
+        interpExp env statement `shouldBe` (env, 13)
+      it "evaluates an expression sequence" $ do
+        let env = (Map.empty, [])
+            statement = EseqExp (AssignStm "a" (NumExp 5)) (OpExp (NumExp 7) Minus (IdExp "a"))
+        interpExp env statement `shouldBe` ((Map.singleton "a" [5], []), 2)
+
+    describe "interpStm" $ do
+      it "evaluates an assign statement" $ do
+        let env = (Map.empty, [])
+            statement = AssignStm "a" (NumExp 5)
+        interpStm env statement `shouldBe` (Map.singleton "a" [5], [])
+      it "evaluates a compound statement, threading the state through" $ do
+        let env = (Map.empty, [])
+            statement = CompoundStm (AssignStm "a" (NumExp 5))
+                                    (AssignStm "b" (NumExp 8))
+            expectedStore = Map.fromList [("a", [5]), ("b", [8])]
+        interpStm env statement `shouldBe` (expectedStore, [])
+      it "evaluates a print statement in order" $ do
+        let env = (Map.empty, [])
+            statement = PrintStm [NumExp 5, NumExp 6, NumExp 7]
+        interpStm env statement `shouldBe` (Map.empty, [5, 6, 7])
+
+    describe "compound statements and expressions" $ do
+      it "returns the most recently assigned value on lookup" $ do
+        let env = (Map.empty, [])
+            statement = EseqExp (CompoundStm (AssignStm "a" (NumExp 5))
+                                             (AssignStm "a" (NumExp 9)))
+                                (IdExp "a")
+        let (_, val) = interpExp env statement
+        val `shouldBe` 9
+      it "threads the updated environments through a print statement" $ do
+        let env = (Map.empty, [])
+            statement =
+              PrintStm [
+                EseqExp (AssignStm "a" (NumExp 5))
+                        (IdExp "a")
+              , EseqExp (AssignStm "a" (OpExp (IdExp "a") Plus (NumExp 6)))
+                        (IdExp "a")
+              , EseqExp (AssignStm "a" (OpExp (IdExp "a") Minus (NumExp 2)))
+                        (IdExp "a")
+              ]
+        let (_, output) = interpStm env statement
+        output `shouldBe` [5, 11, 9]
+      it "prints nested print statement output in order" $ do
+        let env = (Map.empty, [])
+            statement =
+              PrintStm [
+                EseqExp (AssignStm "a" (NumExp 5))
+                        (IdExp "a")
+              , EseqExp (CompoundStm (PrintStm [NumExp 1000])
+                                     (AssignStm "a" (OpExp (IdExp "a") Plus (NumExp 6))))
+                        (IdExp "a")
+              , EseqExp (AssignStm "a" (OpExp (IdExp "a") Minus (NumExp 2)))
+                        (IdExp "a")
+              ]
+        let (_, output) = interpStm env statement
+        output `shouldBe` [5, 1000, 11, 9]
