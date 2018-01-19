d89ad3db804d4723bfb575b7e1f7507db4de1db5
Stuart Terrett <shterrett@gmail.com>
2018-01-19 14:30:42 -0500

    Symbol Table

---
+++ src/Symbol.hs
@@ -0,0 +1,25 @@
+module Symbol where
+
+import qualified Data.Map.Strict as Map
+
+data Symbol = Symbol String Integer
+            deriving (Show)
+
+instance Eq Symbol where
+    (==) (Symbol _ i_1) (Symbol _ i_2) = i_1 == i_2
+
+instance Ord Symbol where
+    compare (Symbol _ i_1) (Symbol _ i_2) = compare i_1 i_2
+
+data SymbolTable = SymbolTable ((Map.Map String Symbol), Integer)
+
+newTable :: Integer -> SymbolTable
+newTable i = SymbolTable (Map.empty, i)
+
+put :: String -> SymbolTable -> SymbolTable
+put s (SymbolTable (m, i)) = SymbolTable ( Map.insertWith (flip const) s (Symbol s i) m
+                                         , i + 1
+                                         )
+
+get :: String -> SymbolTable -> Maybe Symbol
+get s (SymbolTable (m, _)) = Map.lookup s m
---
+++ test/SymbolSpec.hs
@@ -0,0 +1,54 @@
+module SymbolSpec where
+
+import Test.Hspec
+import Test.QuickCheck
+import Data.List (nub, foldl', sort)
+
+import qualified Symbol
+
+prop_equality_by_int :: String -> String -> Integer -> Integer -> Bool
+prop_equality_by_int k_1 k_2 i_1 i_2 =
+    let symbols = [Symbol.Symbol k i | k <- [k_1, k_2],
+                                       i <- [i_1, i_2]]
+        pairs = [(s_1, s_2) | s_1 <- symbols,
+                              s_2 <- symbols]
+    in
+      ((uncurry (==)) <$> pairs)
+        == ((\(Symbol.Symbol _ v_1, Symbol.Symbol _ v_2) -> v_1 == v_2) <$> pairs)
+
+prop_stores_with_integer :: String -> Integer -> Bool
+prop_stores_with_integer key i =
+  (Symbol.get key . Symbol.put key $ Symbol.newTable i)
+    == Just (Symbol.Symbol key i)
+
+prop_ordered_by_integer :: [String] -> [Integer] -> Bool
+prop_ordered_by_integer strings ints =
+    ((\(Symbol.Symbol _ i) -> i) <$> (sort (uncurry Symbol.Symbol <$> zip strings ints)))
+      == sort (take (length strings) ints)
+
+prop_increments :: [String] -> Bool
+prop_increments keys =
+    let unique = nub $ keys
+    in (fmap ((flip Symbol.get) (foldl' (flip Symbol.put) (Symbol.newTable 0) unique))
+             unique
+       )
+              ==
+        ((Just . uncurry Symbol.Symbol) <$> zip unique (iterate (+1) 0))
+
+prop_no_double_increment :: String -> Int -> Bool
+prop_no_double_increment s n =
+    (Symbol.get s $ (foldl' (\t _ -> Symbol.put s t) (Symbol.newTable 0) (replicate ((abs n) + 1) n)))
+      == (Just $ Symbol.Symbol s 0)
+
+spec :: Spec
+spec = do
+    describe "symbol equality" $ do
+      it "is equal when the int portion is equal" $ property prop_equality_by_int
+
+    describe "symbol order" $ do
+      it "orders by the integer in the symbol" $ property prop_ordered_by_integer
+
+    describe "storing strings in symbol table" $ do
+      it "stores the strings with an integer" $ property prop_stores_with_integer
+      it "increments the integer with each addition" $ property prop_increments
+      it "does not increment the value of a symbol if it exists in the table" $ property prop_no_double_increment
